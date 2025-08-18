99969938b99968c0f255d12bb26dc2cc662e97fb
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-18 15:55:32 +0300

    update beck end k8s

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -16,6 +16,37 @@ git remote add origin https://github.com/Eli-M-Fisher/data-loader.git
 git add .
 git commit -m "Initial commit"
 git push -u origin main
+
+oc login --token=.....
+
+oc project eliyahu-dev
+
+oc delete all --all -n eliyahu-dev
+oc delete pvc --all -n eliyahu-dev
+
+docker build -t elif2/data-loader-app:mongo .
+
+docker login -u elif2
+
+docker push elif2/data-loader-app:mongo
+
+oc apply -f infrastructure/k8s/mongo-deployment.yaml
+
+oc get pods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 1. Building and pushing Docker Image with linux/amd64docker login -u elif2
 docker buildx create --use
 docker buildx build --platform linux/amd64 -t elif2/data-loader-app:fixed .
