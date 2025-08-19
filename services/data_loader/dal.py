b105cb45b2ebc2aaf8e2cc35aab7c3f922500b73
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-19 17:16:15 +0300

    finish del

--- services/data_loader/dal.py
+++ services/data_loader/dal.py
@@ -2,20 +2,26 @@ from pymongo import MongoClient
 from .models import Soldier
 
 class SoldierDAL:
-    def __init__(self, host="localhost", port=27017, db_name="enemy_soldiers", 
+    def __init__(self, host="localhost", port=27017, db_name="enemy_soldiers", collection_name="soldier_details"):
+        self.client = MongoClient(host, port)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
 
     def get_all_soldiers(self):
         # get all documents, exclud mongodb internal "_id" field
-        return
+        return list(self.collection.find({}, {"_id": 0}))
 
-    def add_soldier(self):
+    def add_soldier(self, soldier: Soldier):
         # insert a new solder document
-        return
+        self.collection.insert_one(soldier.dict())
+        return {"status": "success", "message": "Soldier added"}
 
-    def update_soldier(self):
+    def update_soldier(self, ID: int, field: str, value: str):
         # update a soldier field by id
-        return
+        result = self.collection.update_one({"ID": ID}, {"$set": {field: value}})
+        return {"status": "success" if result.modified_count > 0 else "failed"}
 
-    def delete_soldier(self):
+    def delete_soldier(self, ID: int):
         # delete soldier by id
-        return
+        result = self.collection.delete_one({"ID": ID})
+        return {"status": "success" if result.deleted_count > 0 else "failed"}
