4f53cdedeebe27a652c8a715d3017c672b0dfc17
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-19 17:22:10 +0300

    finish update db

--- services/data_loader/db.py
+++ services/data_loader/db.py
@@ -2,6 +2,12 @@ from pymongo import MongoClient
 
 class MongoDBClient:
     def __init__(self, host="localhost", port=27017, database="enemy_soldiers", collection="soldier_details"):
+        # create a mongo client
+        self.client = MongoClient(host, port)
+        # select databas
+        self.db = self.client[database]
+        # select collection
+        self.collection = self.db[collection]
 
     def get_collection(self):
         return self.collection
